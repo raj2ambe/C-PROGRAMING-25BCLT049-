01f96f2b77197209019d895cfee75a0ae6cdce8d
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-08-22 11:11:53 +0530

    Delete 4_DivideTwoNumbers.c

--- 4_DivideTwoNumbers.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float a, b, quotient;
-    printf("Enter two numbers: " );
-    scanf("%f %f", &a, &b);
-    quotient = a / b;
-    printf("Quotient = %.2f\n", quotient);
-    return 0;
-}
