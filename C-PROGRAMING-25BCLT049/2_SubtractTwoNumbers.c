4b998507eb57e182763167cae925a0e760db535b
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-08-27 20:38:25 +0530

    Update 2_SubtractTwoNumbers.c

--- C-PROGRAMING-25BCLT049/2_SubtractTwoNumbers.c
+++ C-PROGRAMING-25BCLT049/2_SubtractTwoNumbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int a, b, diff;
-    printf("Enter two numbers: " );
+    printf("Enter two numbers-->" );
     scanf("%d %d", &a, &b);
     diff = a - b;
     printf("Difference = %d\n", diff);
