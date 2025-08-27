dd2e42636ead220dc80ebc3f038a7b4571618534
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-08-27 20:28:17 +0530

    Update 10_DollarsToPounds.c

--- C-PROGRAMING-25BCLT049/10_DollarsToPounds.c
+++ C-PROGRAMING-25BCLT049/10_DollarsToPounds.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     float dollars, pounds;
-    printf("Enter amount in dollars: " );
+    printf("Enter amount in dollars-->" );
     scanf("%f", &dollars);
     pounds = (dollars * 48) / 70;
     printf("Pounds = %.2f\n", pounds);
