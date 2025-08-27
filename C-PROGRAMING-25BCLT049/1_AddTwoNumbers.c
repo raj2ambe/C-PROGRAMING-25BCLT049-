273fbcbcb2c479536fb4cbe1274a9a81db2899d3
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-08-27 20:37:36 +0530

    Update 1_AddTwoNumbers.c

--- C-PROGRAMING-25BCLT049/1_AddTwoNumbers.c
+++ C-PROGRAMING-25BCLT049/1_AddTwoNumbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int a, b, sum;
-    printf("Enter two numbers: " );
+    printf("Enter two numbers-->" );
     scanf("%d %d", &a, &b);
     sum = a + b;
     printf("Sum = %d\n", sum);
