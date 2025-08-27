ae53ae72dc774a2cbce67c5ca41cfc10322b899d
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-08-27 20:39:31 +0530

    Update 3_MultiplyTwoNumbers.c

--- C-PROGRAMING-25BCLT049/3_MultiplyTwoNumbers.c
+++ C-PROGRAMING-25BCLT049/3_MultiplyTwoNumbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int a, b, product;
-    printf("Enter two numbers: " );
+    printf("Enter two numbers-->" );
     scanf("%d %d", &a, &b);
     product = a * b;
     printf("Product = %d\n", product);
